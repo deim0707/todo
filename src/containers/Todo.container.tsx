3d1ec93ff758e06fab49171336fdc0c43ac275c8
Dmitry <dm3iy.p@gmail.com>
2022-02-04 22:13:34 +0300

    replace ErrorBoundary

--- src/containers/Todo.container.tsx
+++ src/containers/Todo.container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {observer} from "mobx-react-lite";
 import todoStore from "../store/todo.store";
 import {TodoList} from "../components/Todo/TodoList";
-import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 const TodoContainer = observer(() => {
     const {todoList, fetchTodos, removeTodo, getTotalTodo, changeTodoCompleted, addTodo} = todoStore;
